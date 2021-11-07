9d76e5bd3be52e98956ba2e83a7ae05cce2c42ec
Borislav Stoyanov <bostoy@protonmail.com>
2021-11-07 13:12:34 +0200

    connect mongodb

---
+++ server/config/database.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+const DB_USER = process.env.DB_USER;
+const DB_PASSWORD = process.env.DB_PASSWORD;
+
+mongoose.connect(`mongodb+srv://${DB_USER}:${DB_PASSWORD}@ecommerce.sa5vg.mongodb.net/ecommerce?retryWrites=true&w=majority`, { useNewUrlParser: true, useUnifiedTopology: true })
+const db = mongoose.connection;
+
+db.on('error', () => console.error('Error connecting to database'));
+db.once('open', () => {
+    console.log('Connected to database');
+});
---
+++ server/index.js
@@ -0,0 +1,11 @@
+require('dotenv').config();
+const express = require('express');
+
+const PORT = process.env.PORT || 8000;
+
+const app = express();
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+});
+

74d91c79fa279b23fead7de998a9fdb963ea5295
Borislav Stoyanov <bostoy@protonmail.com>
2021-11-09 19:13:54 +0200

    remove x-powered-by express header

--- server/index.js
+++ server/index.js
@@ -5,6 +5,8 @@ const router = require('./routes');
 const PORT = process.env.PORT || 8000;
 
 const app = express();
+app.disable('x-powered-by');
+
 
 app.use(express.json()); //Important for parsing application/json
 app.use(router);

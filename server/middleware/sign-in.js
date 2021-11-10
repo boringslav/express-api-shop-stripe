1b347e5598181ba9b75fdd7bd36507e9755e4d05
Borislav Stoyanov <bostoy@protonmail.com>
2021-11-10 11:34:48 +0200

    create login jwt

--- server/middleware/sign-in.js
+++ server/middleware/sign-in.js
@@ -1,22 +1,36 @@
 const CryptoJS = require("crypto-js");
 const User = require('../models/User');
 const authService = require('../services/authService');
+const jwt = require('jsonwebtoken');
 
 module.exports = async (req, res) => {
     const { username, password } = req.body;
 
     try {
         const user = await User.findOne({ username });
-        const decrypted = CryptoJS.AES.decrypt(user.password, process.env.SECRET_KEY).toString(CryptoJS.enc.Utf8);
-
-        if (!user || decrypted !== password) {
+        if (!user) {
             return res.status(401).json({ message: 'Invalid credentials' });
         }
 
+        const decrypted = CryptoJS.AES.decrypt(user.password, process.env.SECRET_KEY).toString(CryptoJS.enc.Utf8);
+
+        if (decrypted !== password) {
+            return res.status(401).json({ message: 'Invalid credentials' });
+        }
+
+        const accessToken = jwt.sign({
+            id: user._id,
+            isAdmin: user.isAdmin
+        },
+            process.env.JWT_SECRET_KEY,
+            { expiresIn: '3d' });
+
+        const adaptedData = Object.entries(user.toObject()).filter(([key, value]) => key !== 'password');
+
         res.status(200).json({
-            //TODO add auth token
-            message: 'Sign in successful',
-        })
+            accessToken,
+            user: Object.fromEntries(adaptedData),
+        });
 
     } catch (err) {
         console.error(err);

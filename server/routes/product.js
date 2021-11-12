e2b40316c36db6e437bdf5bee7bce21f79aea68d
Borislav Stoyanov <bostoy@protonmail.com>
2021-11-12 20:50:32 +0200

    get all products request + filter by category

--- server/routes/product.js
+++ server/routes/product.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const createProduct = require('../middleware/products/createProduct');
 const deleteProduct = require('../middleware/products/deleteProduct');
+const getAllProducts = require('../middleware/products/getAllProducts');
 const getProduct = require('../middleware/products/getProduct');
 const updateProduct = require('../middleware/products/updateProduct');
 const { verifyTokenAndAdmin } = require('../middleware/verifyToken');
@@ -10,6 +11,6 @@ router.route('/:id')
     .put(verifyTokenAndAdmin, updateProduct)
     .delete(verifyTokenAndAdmin, deleteProduct)
 router.post('/create', verifyTokenAndAdmin, createProduct);
-
+router.get('/', getAllProducts)
 
 module.exports = router

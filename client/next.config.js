402e1cab338d792fd96d42560131539c465a9fe8
Borislav Stoyanov <bostoy@protonmail.com>
2021-11-13 16:03:33 +0200

    Finish api + add cors, strapi and make a payment from front end

--- client/next.config.js
+++ client/next.config.js
@@ -1,3 +1,6 @@
 module.exports = {
   reactStrictMode: true,
+  env: {
+    STRIPE_KEY: "pk_test_51JvJmvKxFwz2KomUzFP9zqAhhIc9B9f1naRtEzIAdWhjbF2scnZGREnhdgsF0SayXmLdZHhoXXt18DttcomKquVs00BqbPbb1P"
+  }
 }
---
+++ client/pages/payment.js
@@ -0,0 +1,49 @@
+import { useState, useEffect } from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
+
+function Payment() {
+    const stripeKey = process.env.STRIPE_KEY;
+    const [stripeToken, setStripeToken] = useState('');
+
+    const onToken = (token) => {
+        setStripeToken(token);
+    }
+
+    useEffect(() => {
+        makeRequest();
+    }, [stripeToken]);
+
+
+    const makeRequest = async () => {
+        try {
+            const res = await axios.post("http://localhost:8000/api/checkout/payment", {
+                tokenId: stripeToken.id,
+                amount: 90000
+            });
+            console.log(res.data);
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
+
+    return (
+        <div>
+            <h1>Payment</h1>
+            <StripeCheckout
+                name="Boringshop"
+                billingAddress
+                shippingAddress
+                description="Your total is $90000"
+                amount={9000000} //stripes uses cents for price
+                token={onToken}
+                stripeKey={stripeKey}
+            >
+                <button>Pay</button>
+            </StripeCheckout>
+        </div >
+    )
+}
+
+export default Payment

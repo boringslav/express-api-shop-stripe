5e204723618f7031380bc5bb76863372ac274f21
Borislav Stoyanov <bostoy@protonmail.com>
2021-11-15 14:02:41 +0200

    create navbar

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,11 +1,13 @@
 import '../styles/globals.scss'
 import Head from 'next/head'
+import Navbar from '../components/Navbar'
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
         <title>Boringshop Ecommerce</title>
       </Head>
+      <Navbar />
       <Component {...pageProps} />
     </>
   )

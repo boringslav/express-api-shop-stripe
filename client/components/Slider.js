a183320a11b216b1cd95e502832411067ad54f02
Borislav Stoyanov <bostoy@protonmail.com>
2021-11-17 12:37:41 +0200

    slider functionality

--- client/components/Slider.js
+++ client/components/Slider.js
@@ -1,28 +1,57 @@
 import styles from '../styles/Slider.module.scss';
 import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+import SlideCard from './SlideCard';
+import { useState } from 'react';
+
 function Slider() {
+    const [slideIndex, setSlideIndex] = useState(0);
+    const data = [
+        {
+            id: 1,
+            title: "sport cars",
+            img: "https://www.mercedes-amg.com/content/global/en/vehicles/c-class/coupe/jcr:content/content/tabbedcontent_143413/tabs/tabbedentry_1/content/threeimagegallery_co/image1.media6.responsive_12col_16_9.1624894040905.jpeg",
+            imgAlt: "mercedes c63 amg",
+            description: "The best sport cars!",
+            btnText: "check available sport cars"
+
+        },
+        {
+            id: 2,
+            title: "luxury cars",
+            img: "https://www.ft.com/__origami/service/image/v2/images/raw/https://d1e00ek4ebabms.cloudfront.net/production/7067eefd-4b3c-4ffd-b84f-1b9527fd8c4e.jpg?source=next&fit=scale-down&quality=highest&width=1067",
+            imgAlt: "rolls royce ghost",
+            description: "The most luxury cars",
+            btnText: "check available luxury cars"
+
+        },
+    ]
+
+    let additionalStyles = {
+        transform: `translateX(${slideIndex * -100}vw)`
+    }
+
+    const handleClick = (direction) => {
+        if (direction === 'left') {
+            setSlideIndex(slideIndex > 0 ? slideIndex - 1 : data.length - 1);
+        } else {
+            setSlideIndex(slideIndex < data.length - 1 ? slideIndex + 1 : 0);
+        }
+    }
+
     return (
         <div className={styles.container}>
             <div className={styles.arrowLeft}>
-                <ArrowBackIosNewIcon />
+                <ArrowBackIosNewIcon onClick={() => handleClick('left')} />
             </div>
-            <div className={styles.wrapper}>
+            <div className={styles.wrapper} style={additionalStyles}>
+                {data.map((el) => {
+                    return <SlideCard {...el} />
+                })}
 
-                <div className={styles.slide}>
-                    <div className={styles.imgContainer}>
-                        <img src="https://www.mercedes-amg.com/content/global/en/vehicles/c-class/coupe/jcr:content/content/tabbedcontent_143413/tabs/tabbedentry_1/content/threeimagegallery_co/image1.media6.responsive_12col_16_9.1624894040905.jpeg" alt="mercedes c63 amg" />
-                    </div>
-                    <div className={styles.infoContainer}>
-                        <h1>sport cars</h1>
-                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. A quidem maiores mollitia exercitationem, cupiditate beatae officiis dolore odio provident eum aspernatur iste eligendi quibusdam, dolores perspiciatis accusantium in tempore! Rem est fugit exercitationem. Optio repudiandae maiores vero corporis fugit cumque. Voluptate nisi veniam consequuntur odit quia natus expedita ullam odio amet corporis, soluta, quod qui! Harum cumque rem dolores ea eaque temporibus nemo corrupti itaque nihil aliquid ut libero inventore, esse saepe reprehenderit perferendis, aperiam vitae. Vitae aliquam aliquid inventore voluptates nesciunt est cumque eveniet provident, dolor officiis explicabo consequuntur excepturi vero ipsum quas quos voluptatem iusto quaerat voluptate accusantium!</p>
-                        <button>check available cars</button>
-                    </div>
-                </div>
             </div>
-
             <div className={styles.arrowRight}>
-                <ArrowForwardIosIcon />
+                <ArrowForwardIosIcon onClick={() => handleClick('right')} />
             </div>
         </div>
     )
